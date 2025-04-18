0778774221d0bb41ed96bce1def7335288104793
RedGfox <zim238kosmit@gmail.com>
2025-04-18 11:42:39 +0300

    hw1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const categoriesList = document.querySelector(`#categories`);
+const categoryItem = categoriesList.querySelectorAll(`.item`);
+console.log(`Number of categories: ${categoryItem.length}`);
+categoryItem.forEach(item => {
+  const title = item.querySelector(`h2`).textContent;
+  const elementsCount = item.querySelectorAll(`ul li`).length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
